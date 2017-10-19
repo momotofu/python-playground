9775205dea2ba89c047254a8d2cafb710709062f
Christopher Reece <mrkita.reece@gmail.com>
2017-10-19 22:59:11 +0900

    refactored loop

--- fs_nano_degree/taking_a_break.py
+++ fs_nano_degree/taking_a_break.py
@@ -1,6 +1,10 @@
 import webbrowser
 import time
 
-for num in range(0, 3):
+total_breaks = 3
+break_count = 0
+
+while(break_count < total_breaks):
     time.sleep(1)
     webbrowser.open("https://www.youtube.com/watch?v=WjZ01FcK0yk")
+    break_count++
