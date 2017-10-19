c5d2853fcf12eff5c21fc68b129aeaf99a731a1f
Christopher Reece <mrkita.reece@gmail.com>
2017-10-19 22:55:15 +0900

    added a loop

--- fs_nano_degree/taking_a_break.py
+++ fs_nano_degree/taking_a_break.py
@@ -1,5 +1,6 @@
 import webbrowser
 import time
 
-time.sleep(10)
-webbrowser.open("https://www.youtube.com/watch?v=WjZ01FcK0yk")
+for num in range(0..2):
+    time.sleep(1)
+    webbrowser.open("https://www.youtube.com/watch?v=WjZ01FcK0yk")
