c1b733c2c73de926809273f62f14b77eed259baf
Christopher Reece <mrkita.reece@gmail.com>
2017-10-19 23:01:55 +0900

    print out time when program starts

--- fs_nano_degree/taking_a_break.py
+++ fs_nano_degree/taking_a_break.py
@@ -4,6 +4,8 @@ import time
 total_breaks = 3
 break_count = 0
 
+print("This program started on {0}".format(time.ctime())
+
 while(break_count < total_breaks):
     time.sleep(1)
     webbrowser.open("https://www.youtube.com/watch?v=WjZ01FcK0yk")
