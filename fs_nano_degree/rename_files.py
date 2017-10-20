c03e1b8387778508507192f1c88b9396aefc32ca
Christopher Reece <mrkita.reece@gmail.com>
2017-10-20 13:04:15 +0900

    tested out os login

--- fs_nano_degree/rename_files.py
+++ fs_nano_degree/rename_files.py
@@ -1,5 +1,11 @@
-def rename_files():
-    # (1) get file names from a folder
-    # (2) for each file, rename filename
+import os
 
-rename_files()
+print('login: ' + os.getlogin())
+# def rename_files():
+#      (1) get file names from a folder
+#      file_list = os.listdir("/Users/christopherreece/Documents/Github/Python/fs_nano_degree/prank")
+#     print(file_list)
+#
+#      (2) for each file, rename filename
+#
+#  rename_files()
