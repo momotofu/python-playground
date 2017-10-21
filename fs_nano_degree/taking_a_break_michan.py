baf13572333ca93560ffa62d1e0af4ce40867869
Christopher Reece <mrkita.reece@gmail.com>
2017-10-21 11:41:57 +0900

    commit

--- fs_nano_degree/taking_a_break_michan.py
+++ fs_nano_degree/taking_a_break_michan.py
@@ -7,8 +7,13 @@ import webbrowser
 url='https://www.google.co.jp/search?q=cute+pomeranian&safe=active&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiWzIXK8f7WAhUMwrwKHUhsBhoQ_AUICigB&biw=1440&bih=777'
 
 # repeat open a window every one hour ten times
-ten=10
-while ten >= 0:
+pie=5
+
+
+while pie > 0:
     webbrowser.open(url)
-    time.sleep(2)
+    time.sleep(60*60)
+    pie = pie - 1
+    print("pie: " + str(pie))
+
 #open a window
