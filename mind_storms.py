452b2cbd8aa00b74627a99b086e915bf6bd7d364
Christopher Reece <mrkita.reece@gmail.com>
2017-10-23 23:49:58 +0900

    added open_window function

--- mind_storms.py
+++ mind_storms.py
@@ -2,18 +2,27 @@
 
 import turtle
 
+
 def draw_square():
-    window = turtle.Screen()
-    window.bgcolor("yellow")
 
     brad = turtle.Turtle()
     brad.shape("turtle")
     brad.color("green")
     brad.speed(2)
 
-    for i in range(0,4):
-        brad.forward(100)
-        brad.right(90)
+    for i in range(0, 100):
+        brad.right(30)
+        brad.forward(16)
+        for i in range(0,4):
+            brad.forward(100)
+            brad.right(90)
 
+def open_window():
+    window = turtle.Screen()
+    window.bgcolor("yellow")
     window.exitonclick()
+
+open_window()
 draw_square()
+
+
