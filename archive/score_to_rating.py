035506a2a920122f972fc76dfbe33b80ecc74e2e
Christopher Reece <mrkita.reece@gmail.com>
2017-10-23 16:30:41 +0900

    fixed more formatting bugs

--- archive/score_to_rating.py
+++ archive/score_to_rating.py
@@ -1,5 +1,4 @@
-
-import functools import reduce
+from functools import reduce
 
 def convert_to_numeric(input):
     return float(input)
@@ -12,21 +11,21 @@ def sum_of_middle_three(n1, n2, n3, n4, n5):
     return reduce((lambda x, y: x + y), list)
 
 def score_to_rating_string(score):
-"""
-Takes a numerical input and maps its value to
-the correct rating. That rating is then returned.
-"""
+    """
+    Takes a numerical input and maps its value to
+    the correct rating. That rating is then returned.
+    """
 
-if 0 <= score < 1:
-    return "Terrible"
-elif 1 <= score < 2:
-    return "Bad"
-elif 2 <= score < 3:
-    return "OK"
-elif 3 <= score < 4:
-    return "Good"
-elif 4 <= score <= 5:
-    return "Excellent"
+    if 0 <= score < 1:
+        return "Terrible"
+    elif 1 <= score < 2:
+        return "Bad"
+    elif 2 <= score < 3:
+        return "OK"
+    elif 3 <= score < 4:
+        return "Good"
+    elif 4 <= score <= 5:
+        return "Excellent"
 
 def scores_to_rating(n1, n2, n3, n4, n5):
     in1 = convert_to_numeric(n1)
