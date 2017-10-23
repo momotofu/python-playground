5f4cfa1d65c9ec0f3dbaabfdb80a4cefe6ee519b
Christopher Reece <mrkita.reece@gmail.com>
2017-10-23 23:01:10 +0900

    successfully drew a square

--- mind_storms.py
+++ mind_storms.py
@@ -7,6 +7,10 @@ def draw_square():
     window.bgcolor("yellow")
 
     brad = turtle.Turtle()
+    brad.shape("turtle")
+    brad.color("green")
+    brad.speed(2)
+
     for i in range(0,4):
         brad.forward(100)
         brad.right(90)
