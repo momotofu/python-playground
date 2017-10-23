adb83eaca08a2246326e19193ad3312b3bae3c3a
Christopher Reece <mrkita.reece@gmail.com>
2017-10-23 16:28:24 +0900

    formated code blocks

--- archive/score_to_rating.py
+++ archive/score_to_rating.py
@@ -6,33 +6,34 @@ def convert_to_numeric(input):
 
 def sum_of_middle_three(n1, n2, n3, n4, n5):
     list = [n1, n2, n3, n4, n5]
-                list.remove(max(list))
-                    list.remove(min(list))
+    list.remove(max(list))
+    list.remove(min(list))
 
-                            return reduce((lambda x, y: x + y), list)
+    return reduce((lambda x, y: x + y), list)
 
-                        def score_to_rating_string(score):
-                            """
-                                    Takes a numerical input and maps its value to
-                                        the correct rating. That rating is then returned.
-                                            """
-                                                if 0 <= score < 1:
-                                                    return "Terrible"
-                                                elif 1 <= score < 2:
-                                                    return "Bad"
-                                                elif 2 <= score < 3:
-                                                    return "OK"
-                                                elif 3 <= score < 4:
-                                                    return "Good"
-                                                elif 4 <= score <= 5:
-                                                    return "Excellent"
+def score_to_rating_string(score):
+"""
+Takes a numerical input and maps its value to
+the correct rating. That rating is then returned.
+"""
 
-                                                def scores_to_rating(n1, n2, n3, n4, n5):
-                                                    in1 = convert_to_numeric(n1)
-                                                                                                                    in2 = convert_to_numeric(n2)
-                                                                                                                        in3 = convert_to_numeric(n3)
-                                                                                                                            in4 = convert_to_numeric(n4)
-                                                                                                                                in5 = convert_to_numeric(n5)
+if 0 <= score < 1:
+    return "Terrible"
+elif 1 <= score < 2:
+    return "Bad"
+elif 2 <= score < 3:
+    return "OK"
+elif 3 <= score < 4:
+    return "Good"
+elif 4 <= score <= 5:
+    return "Excellent"
 
-                                                                                                                                        average = sum_of_middle_three(in1, in2, in3, in4, in5)/3
-                                                                                                                                            return score_to_rating_string(average)
+def scores_to_rating(n1, n2, n3, n4, n5):
+    in1 = convert_to_numeric(n1)
+    in2 = convert_to_numeric(n2)
+    in3 = convert_to_numeric(n3)
+    in4 = convert_to_numeric(n4)
+    in5 = convert_to_numeric(n5)
+
+    average = sum_of_middle_three(in1, in2, in3, in4, in5)/3
+    return score_to_rating_string(average)
