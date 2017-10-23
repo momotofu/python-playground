e49cd1242e788a05d1c27ca36c038310264b7044
Christopher Reece <mrkita.reece@gmail.com>
2017-10-23 22:53:47 +0900

    filled out method

--- mind_storms.py
+++ mind_storms.py
@@ -1,1 +1,13 @@
 #Class practice
+
+import turtle
+
+def draw_square():
+    window = turtle.Screen()
+    window.bgcolor("red")
+
+    brad = turtle.Turtle()
+    brad.foward(100)
+
+    window.closeonclick()
+draw_square()
