4e089c4e5893e002f164b6f5473db4cb8907f14c
Christopher Reece <mrkita.reece@gmail.com>
2017-10-26 13:06:50 +0900

    added new creds

--- fs_nano_degree/twilio/send_text.py
+++ fs_nano_degree/twilio/send_text.py
@@ -1,16 +1,16 @@
 
-import twilio.rest import Client
+from twilio.rest import Client
 
 # Your Account SID from twilio.com/console
 account_sid = "ACb6ed0a80b9b770fc7d11ee41341a65c2"
 # Your Auth Token from twilio.com/console
-auth_token  "3f4aa788221625f1150637d7e04e12f4"
+auth_token = "3f4aa788221625f1150637d7e04e12f4"
 
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
-    to="+810703840886",
-    from_="+81345108112",
-    body="I am right behind you... Mizuho")
+    to="+14358622097",
+    from_="+14352362349",
+    body="Hey Dad, it's Chris. I'm working on a programming project. If you get this message let me know on facebook messenger. Have a good day! :D")
 
 print(message.sid)
