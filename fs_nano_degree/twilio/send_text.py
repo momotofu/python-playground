3a8044bce9c69c530a7a166c9ccc188ed68a2f40
Christopher Reece <mrkita.reece@gmail.com>
2017-10-26 12:41:21 +0900

    added sid and auth token creds

--- fs_nano_degree/twilio/send_text.py
+++ fs_nano_degree/twilio/send_text.py
@@ -4,7 +4,7 @@ m twilio.rest import Client
 # Your Account SID from twilio.com/console
 account_sid = "ACb6ed0a80b9b770fc7d11ee41341a65c2"
 # Your Auth Token from twilio.com/console
-auth_token  = "your_auth_token"
+auth_token  "3f4aa788221625f1150637d7e04e12f4"
 
 client = Client(account_sid, auth_token)
 
