226d1addcaac9e41ed99d93437744980321d4281
Christopher Reece <mrkita.reece@gmail.com>
2017-10-28 18:48:37 +0900

    refacted read_file function

--- fs_nano_degree/profanity_editor.py
+++ fs_nano_degree/profanity_editor.py
@@ -1,10 +1,11 @@
 def read_text():
-   text =  open("./movie_text.txt")
-   contents_of_file = text.read()
-   print("text:", contents_of_file)
-   text.close()
+    text =  open("./movie_text.txt")
+    contents_of_file = text.read()
+    text.close()
+    return contents_of_file
 
 def check_profanity(text_to_check):
     has_profanity = urllib.urlopen("http://www.wdyl.com/profanity?q="+text_to_check)
+    return has_profanity
 
 read_text()
