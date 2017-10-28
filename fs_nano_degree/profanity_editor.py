badfacbce73ab033cdec5bd43d5a1b5b515be147
Christopher Reece <mrkita.reece@gmail.com>
2017-10-28 20:30:00 +0900

    established connection

--- fs_nano_degree/profanity_editor.py
+++ fs_nano_degree/profanity_editor.py
@@ -7,8 +7,8 @@ def read_text():
     return contents_of_file
 
 def check_profanity(text_to_check):
-    print(urlopen("http://http://www.wdyl.com/profanity?q="+text_to_check).geturl())
-    return has_profanity
+    print(urlopen("http://www.wdylike.appspot.com/?q="+text_to_check).info())
+    # return has_profanity
 
-check_profanity(shit)
+check_profanity('shet')
 # check_profanity(read_text())
