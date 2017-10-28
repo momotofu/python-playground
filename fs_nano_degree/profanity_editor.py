2e43933d2e8bda0ebeb59d0ea5ea8d487659dbd0
Christopher Reece <mrkita.reece@gmail.com>
2017-10-28 18:46:46 +0900

    added check profanity function

--- fs_nano_degree/profanity_editor.py
+++ fs_nano_degree/profanity_editor.py
@@ -3,4 +3,8 @@ def read_text():
    contents_of_file = text.read()
    print("text:", contents_of_file)
    text.close()
+
+def check_profanity(text_to_check):
+    has_profanity = urllib.urlopen("http://www.wdyl.com/profanity?q="+text_to_check)
+
 read_text()
