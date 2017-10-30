594d8297dad5247101e23e843172c0b161b9dd5e
Christopher Reece <mrkita.reece@gmail.com>
2017-10-30 14:14:30 +0900

    class is alive!

--- fs_nano_degree/movie_website/entertainment_center.py
+++ fs_nano_degree/movie_website/entertainment_center.py
@@ -4,3 +4,5 @@ toy_story = media.Movie("Toy Story",
         "A boy whose toys come to life",
         "https://en.wikipedia.org/wiki/File:Toy_Story.jpg",
         "https://www.youtube.com/watch?v=KYz2wyBy3kc")
+
+print(toy_story.poster_image_url)
