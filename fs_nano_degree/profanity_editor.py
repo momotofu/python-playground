0521f0973476b7dfd1cc969f606c1b2ea408367b
Christopher Reece <mrkita.reece@gmail.com>
2017-10-30 11:17:10 +0900

    got profanity editor working

--- fs_nano_degree/profanity_editor.py
+++ fs_nano_degree/profanity_editor.py
@@ -1,4 +1,5 @@
 from urllib.request import urlopen
+from urllib.parse import urlencode
 
 def read_text():
     text =  open("./movie_text.txt")
@@ -7,8 +8,13 @@ def read_text():
     return contents_of_file
 
 def check_profanity(text_to_check):
-    print(urlopen("http://www.wdylike.appspot.com/?q="+text_to_check).info())
-    # return has_profanity
+    is_profane = False
+    params = urlencode({'q': text_to_check})
+    connection = urlopen("http://www.wdylike.appspot.com/?q="+params);
+    is_profane = connection.read().decode("utf-8") == 'true'
+    connection.close()
+    return is_profane
 
-check_profanity('shet')
+content = read_text()
+print(check_profanity(content))
 # check_profanity(read_text())
