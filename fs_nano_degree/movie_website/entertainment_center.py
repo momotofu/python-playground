81941b55d0bec1424549248975f5309331cdcba8
Christopher Reece <mrkita.reece@gmail.com>
2017-10-30 14:28:09 +0900

    class is alive!

--- fs_nano_degree/movie_website/entertainment_center.py
+++ fs_nano_degree/movie_website/entertainment_center.py
@@ -5,4 +5,8 @@ toy_story = media.Movie("Toy Story",
         "https://en.wikipedia.org/wiki/File:Toy_Story.jpg",
         "https://www.youtube.com/watch?v=KYz2wyBy3kc")
 
-print(toy_story.poster_image_url)
+avatar = media.Movie("Avatar",
+        "A marine on an alien planet",
+        "http://www.imdb.com/title/tt0499549/videoplayer/vi531039513?ref_=tt_ov_vi",
+        "https://images-na.ssl-images-amazon.com/images/M/MV5BMTYwOTEwNjAzMl5BMl5BanBnXkFtZTcwODc5MTUwMw@@._V1_.jpg")
+print(avatar.poster_image_url)
