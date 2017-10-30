277c0b60bde929eb02eac281e028747a2c3adb62
Christopher Reece <mrkita.reece@gmail.com>
2017-10-30 14:02:13 +0900

    added parameters to class init

--- fs_nano_degree/movie_website/media.py
+++ fs_nano_degree/movie_website/media.py
@@ -1,6 +1,6 @@
 class Movie():
-    def __init__(self):
-        self.title
-        self.storyline
-        self.poster_image_url
-        self.trailer_youtube_url
+    def __init__(self, title, storyline, poster_image_url, trailer_url):
+        self.title = title
+        self.storyline = storyline
+        self.poster_image_url = poster_image_url
+        self.trailer_url = trailer_url
