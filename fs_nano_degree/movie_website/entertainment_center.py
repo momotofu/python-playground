fbdacacefc654c0f7b752cb755ba9ac223c6bd3c
Christopher Reece <mrkita.reece@gmail.com>
2017-10-30 14:09:53 +0900

    added arguments to class instance

--- fs_nano_degree/movie_website/entertainment_center.py
+++ fs_nano_degree/movie_website/entertainment_center.py
@@ -1,3 +1,6 @@
 import media
 
-toy_story = media.Movie()
+toy_story = media.Movie("Toy Story",
+        "A boy whose toys come to life",
+        "https://en.wikipedia.org/wiki/File:Toy_Story.jpg",
+        "https://www.youtube.com/watch?v=KYz2wyBy3kc")
