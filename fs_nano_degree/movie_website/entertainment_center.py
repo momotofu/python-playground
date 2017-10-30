b5e2e745d4e4fb702c5a420e711be3852792eb91
Christopher Reece <mrkita.reece@gmail.com>
2017-10-30 15:41:48 +0900

    aww yeah

--- fs_nano_degree/movie_website/entertainment_center.py
+++ fs_nano_degree/movie_website/entertainment_center.py
@@ -1,13 +1,20 @@
 import media
+import fresh_tomatoes
 
 toy_story = media.Movie("Toy Story",
         "A boy whose toys come to life",
-        "https://en.wikipedia.org/wiki/File:Toy_Story.jpg",
+        "http://i0.kym-cdn.com/photos/images/original/001/045/043/cdc.jpg",
         "https://www.youtube.com/watch?v=KYz2wyBy3kc")
 
 avatar = media.Movie("Avatar",
         "A marine on an alien planet",
-        "http://www.imdb.com/title/tt0499549/videoplayer/vi531039513?ref_=tt_ov_vi",
-        "https://images-na.ssl-images-amazon.com/images/M/MV5BMTYwOTEwNjAzMl5BMl5BanBnXkFtZTcwODc5MTUwMw@@._V1_.jpg")
-print(avatar.poster_image_url)
-avatar.show_trailer()
+        "https://images-na.ssl-images-amazon.com/images/M/MV5BMTYwOTEwNjAzMl5BMl5BanBnXkFtZTcwODc5MTUwMw@@._V1_.jpg",
+        "https://www.youtube.com/watch?v=5PSNL1qE6VY")
+
+planet_earth = media.Movie("Planet Earth", "A visual journey covering the wonders of planet Earth",
+        "https://i.pinimg.com/736x/98/2f/cc/982fcc3212d1dab93f99bee02725c9cc--planet-earth-documentary-bbc-planet-earth.jpg",
+        "https://www.youtube.com/watch?v=tiVNk6_0GdY")
+
+movies_list = [planet_earth, avatar, toy_story]
+
+fresh_tomatoes.open_movies_page(movies_list)
--- fs_nano_degree/movie_website/media.py
+++ fs_nano_degree/movie_website/media.py
@@ -5,7 +5,7 @@ class Movie():
         self.title = title
         self.storyline = storyline
         self.poster_image_url = poster_image_url
-        self.trailer_url = trailer_url
+        self.trailer_youtube_url = trailer_url
 
     def show_trailer(self):
         webbrowser.open(self.trailer_url)
