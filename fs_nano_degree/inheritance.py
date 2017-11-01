da04a1e1ecd09270d02b8a36fbb4b1d79fd1116c
Christopher Reece <mrkita.reece@gmail.com>
2017-11-01 17:41:08 +0900

    inheriting methods

--- fs_nano_degree/inheritance.py
+++ fs_nano_degree/inheritance.py
@@ -3,6 +3,11 @@ class Parent():
         print("Parent constructor called")
         self.last_name = last_name
         self.eye_color = eye_color
+
+    def show_info():
+        print("Last name - "+self.last_name)
+        print("Eye color - "+self.eye_color)
+
 class Child(Parent):
     def __init__(self, last_name, eye_color, number_of_toys):
         print("Child constructor called")
@@ -11,7 +16,7 @@ class Child(Parent):
 
 king_david = Parent("David", "blue")
 king_solomon = Child("David", "green", "100000")
-
-print(king_david.eye_color)
-print(king_solomon.eye_color, king_solomon.number_of_toys)
+# print(king_david.eye_color)
+# print(king_solomon.eye_color, king_solomon.number_of_toys)
+king_solomon.show_info()
 
