363b4a9b15e844914cc9d111ea6d2d5679055494
Christopher Reece <mrkita.reece@gmail.com>
2017-11-01 12:24:35 +0900

    added prince of egypt movie data

--- fs_nano_degree/movie_website/entertainment_center.py
+++ fs_nano_degree/movie_website/entertainment_center.py
@@ -1,19 +1,16 @@
 import media
 import fresh_tomatoes
 
-toy_story = media.Movie("Toy Story",
-        "A boy whose toys come to life",
-        "http://i0.kym-cdn.com/photos/images/original/001/045/043/cdc.jpg",
-        "https://www.youtube.com/watch?v=KYz2wyBy3kc")
-
-avatar = media.Movie("Avatar",
-        "A marine on an alien planet",
-        "https://images-na.ssl-images-amazon.com/images/M/MV5BMTYwOTEwNjAzMl5BMl5BanBnXkFtZTcwODc5MTUwMw@@._V1_.jpg",
-        "https://www.youtube.com/watch?v=5PSNL1qE6VY")
-
 planet_earth = media.Movie("Planet Earth", "A visual journey covering the wonders of planet Earth",
         "https://i.pinimg.com/736x/98/2f/cc/982fcc3212d1dab93f99bee02725c9cc--planet-earth-documentary-bbc-planet-earth.jpg",
         "https://www.youtube.com/watch?v=tiVNk6_0GdY")
+
+prince_of_egypt = media.Movie("Prince of Egypt",
+        "An Egyptian prince learns of his identity as a Hebrew and his destiny to become the chosen deliverer of his people.",
+        "https://www.google.co.jp/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwj_56vl4ZrXAhUEe7wKHdhxA4sQjRwIBw&url=http%3A%2F%2Fwww.imdb.com%2Ftitle%2Ftt0120794%2F&psig=AOvVaw2ed_BR4H7FTp1ZEF85b1g8&ust=1509533146821359",
+        "https://www.youtube.com/watch?v=zmBDY-fSlIs")
+
+
 
 movies_list = [planet_earth, avatar, toy_story]
 
