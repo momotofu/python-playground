6c5fe0a9762c7a9cbc7ec0767a9df6bad6b728bd
Christopher Reece <mrkita.reece@gmail.com>
2017-11-01 14:40:04 +0900

    tested out __name__ and __module__

---
+++ fs_nano_degree/name_module_practice.py
@@ -0,0 +1,10 @@
+
+class Faith:
+    """
+    A class to store the simple aspects of faith
+    """
+    def hope():
+        print("Belief in an inheritance in the Kingdom of God")
+
+# print(Faith.__name__)
+# print(Faith.__module__)
--- fs_nano_degree/test.py
+++ fs_nano_degree/test.py
@@ -1,6 +1,12 @@
-times_to_repeat = 3
+# times_to_repeat = 3
+#
+# while times_to_repeat >= 0:
+#     addr = input()
+#     print(addr)
+#     times_to_repeat -= 1
 
-while times_to_repeat >= 0:
-    addr = input()
-    print(addr)
-    times_to_repeat -= 1
+from name_module_practice import Faith
+
+print(Faith.__module__)
+print(Faith.__name__)
+print(Faith.__doc__)
