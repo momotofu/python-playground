7e7000ae921c86ffe5e775ee451296fa79cb7cf4
Christopher Reece <mrkita.reece@gmail.com>
2017-11-02 16:19:36 +0900

    working with tuples

---
+++ intro_to_py/tuples.py
@@ -0,0 +1,6 @@
+dimensions = 15, 14, 100, 33, 459, 10, 60
+print(dimensions)
+a, b, c, d, e, f, g = dimensions
+print(a, b, c, d, e, f, g)
+print('The dimensions are: {} : {} : {} : {} : {} : {} : {}'.format(a, b, c, d, e, f, g))
+
