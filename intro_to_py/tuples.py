f500e59e01328de38a83614ad5cdd442134d4b30
Christopher Reece <mrkita.reece@gmail.com>
2017-11-02 16:22:45 +0900

    added first and last method

--- intro_to_py/tuples.py
+++ intro_to_py/tuples.py
@@ -1,6 +1,12 @@
 dimensions = 15, 14, 100, 33, 459, 10, 60
-print(dimensions)
+# print(dimensions)
 a, b, c, d, e, f, g = dimensions
-print(a, b, c, d, e, f, g)
-print('The dimensions are: {} : {} : {} : {} : {} : {} : {}'.format(a, b, c, d, e, f, g))
+# print(a, b, c, d, e, f, g)
+# print('The dimensions are: {} : {} : {} : {} : {} : {} : {}'.format(a, b, c, d, e, f, g))
 
+# Can use tuples as keys for a dict
+
+def first_and_last(sequence):
+    return sequence[0], sequence[-1]
+
+print(first_and_last((a, b, c, d, e, f, g)))
