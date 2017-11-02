1b9969a354e66c63c956301eb4edb5e58a859995
Christopher Reece <mrkita.reece@gmail.com>
2017-11-03 00:09:18 +0900

    showed off some code

--- fs_nano_degree/taking_a_break.py
+++ fs_nano_degree/taking_a_break.py
@@ -7,6 +7,6 @@ break_count = 0
 print("This program started on {0}".format(time.ctime()))
 
 while(break_count < total_breaks):
-    time.sleep(1)
+    time.sleep(10)
     webbrowser.open("https://www.youtube.com/watch?v=WjZ01FcK0yk")
     break_count += 1
