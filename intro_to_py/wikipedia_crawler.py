59a440d00b95afbd97d9e9f358231901234d0400
Christopher Reece <mrkita.reece@gmail.com>
2017-11-07 17:39:44 +0900

    added hardcoded target and start page

--- intro_to_py/wikipedia_crawler.py
+++ intro_to_py/wikipedia_crawler.py
@@ -4,4 +4,6 @@ import urllib
 from bs4 import BeautifulSoup as bs
 import requests
 
+start_url = "https://en.wikipedia.org/wiki/Special:Random"
+target_url = "https://en.wikipedia.org/wiki/Philosophy"
 
