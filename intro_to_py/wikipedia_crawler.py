a834267292fc7fbe828fc5d9786987691c7740e4
Christopher Reece <mrkita.reece@gmail.com>
2017-11-07 17:40:46 +0900

    started find_first_link func

--- intro_to_py/wikipedia_crawler.py
+++ intro_to_py/wikipedia_crawler.py
@@ -7,3 +7,9 @@ import requests
 start_url = "https://en.wikipedia.org/wiki/Special:Random"
 target_url = "https://en.wikipedia.org/wiki/Philosophy"
 
+def find_first_link(url):
+    res = requests.get(url)
+    html = res.text
+    soup = bs(html, "html.parser")
+
+
