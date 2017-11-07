99011e526bbfd3a90f7c81bb88293871b10a41a0
Christopher Reece <mrkita.reece@gmail.com>
2017-11-07 17:42:31 +0900

    added lovely comments

--- intro_to_py/wikipedia_crawler.py
+++ intro_to_py/wikipedia_crawler.py
@@ -12,4 +12,9 @@ def find_first_link(url):
     html = res.text
     soup = bs(html, "html.parser")
 
+    # This div dontains the article's body
+    # (June 2017 Note: Body nested in two div tags)
+    content_div = soup.find(id="mw-content-text").find(class_="mw-parser-output")
 
+    # stores the first link found in the article, if the article contains no
+    # links this value will ramain None
