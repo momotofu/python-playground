07b6d830a95d6742cc9487ccbd7b88cdd22f0206
Christopher Reece <mrkita.reece@gmail.com>
2017-11-18 18:57:25 +0900

    added substring functions

--- archive/count_substrings.py
+++ archive/count_substrings.py
@@ -6,3 +6,19 @@ def test_func_speed(func):
     finish = datetime.datetime.now()
     print finish - start
 
+def count_substring_b(string, sub_string):
+    sum = 0
+    index = 0
+    while(index < len(string)):
+        matchIndex = string.find(sub_string, index)
+        if matchIndex != -1:
+            index = matchIndex + 1
+            sum += 1
+        else:
+            break
+    return sum
+
+def count_substring_a(string, substring):
+    return sum([ 1 for _ in range(len(string)-len(substring)+1) if string[i:i+len(substring)] == substring]))
+
+
