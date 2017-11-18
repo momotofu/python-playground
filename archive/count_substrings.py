12eba22ad50165edfdb8cf19226f6ad56e1e9fc8
Christopher Reece <mrkita.reece@gmail.com>
2017-11-18 19:00:16 +0900

    created arbitrary "large" input

--- archive/count_substrings.py
+++ archive/count_substrings.py
@@ -21,4 +21,11 @@ def count_substring_b(string, sub_string):
 def count_substring_a(string, substring):
     return sum([ 1 for _ in range(len(string)-len(substring)+1) if string[i:i+len(substring)] == substring]))
 
+string = 'aabsabasdbabbasdbadifjaasdaboibaijdaodiabjabasdibaosidjbaosdibj'
+        +'aabsabasdbabbasdbadifjaasdaboibaijdaodiabjabasdibaosidjbaosdibj'
+        +'aabsabasdbabbasdbadifjaasdaboibaijdaodiabjabasdibaosidjbaosdibj'
+        +'aabsabasdbabbasdbadifjaasdaboibaijdaodiabjabasdibaosidjbaosdibj'
 
+substring = 'ab'
+print 'a'
+test_func_speed(count_substing_a(
